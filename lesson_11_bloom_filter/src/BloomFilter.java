a12f4f3300e01bd6f2fcc80c8d0cb7ca3e1064c2
marchelos <markkolp10@gmail.com>
2023-01-07 03:09:26 +0300

    lesson 11. Impl BloomFilter and tests

---
+++ lesson_11_bloom_filter/src/BloomFilter.java
@@ -0,0 +1,42 @@
+public class BloomFilter {
+    public int filter_len;
+    private int bitmask;
+
+    public BloomFilter(int f_len) {
+        filter_len = f_len;
+        bitmask = 0;
+    }
+
+    public int hash1(String str1) {
+        int hash = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            int code = (int) str1.charAt(i);
+            hash = (hash * 17) + code;
+        }
+
+        return hash % filter_len;
+    }
+
+    public int hash2(String str1) {
+        int hash = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            int code = (int) str1.charAt(i);
+            hash = (hash * 223) + code;
+        }
+
+        return hash % filter_len;
+    }
+
+    public void add(String str1) {
+        int ind1 = hash1(str1);
+        int ind2 = hash2(str1);
+        bitmask = (bitmask | (1 << ind1)) | (bitmask | (1 << ind2));
+    }
+
+    public boolean isValue(String str1) {
+        final int ind1 = hash1(str1);
+        final int ind2 = hash2(str1);
+
+        return ((bitmask & (1 << ind1)) != 0) && ((bitmask & (1 << ind2)) != 0);
+    }
+}
