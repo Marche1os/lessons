a3a7673f37d57ef52e69260b1e45ad89bc0c411d
marchelos <markkolp10@gmail.com>
2023-01-18 15:49:39 +0300

    recursion. fix number 4

--- recursion/src/Polindrom.java
+++ recursion/src/Polindrom.java
@@ -1,17 +1,14 @@
 public class Polindrom {
 
-    public static boolean isPolindromString(final String inputString) {
-        if (inputString.length() < 2)
+    public static boolean isPolindrom(final String s) {
+        if (s.length() < 2) {
             return true;
-
-        return inputString.equalsIgnoreCase(reversedString(inputString, inputString.length()));
-    }
-
-    private static String reversedString(final String inputString, int stringSize) {
-        if (stringSize == 1) {
-            return inputString.substring(0, 1);
-        } else {
-            return inputString.charAt(stringSize - 1) + reversedString(inputString, stringSize - 1);
         }
+
+        if (s.charAt(0) == s.charAt(s.length() - 1)) {
+            return isPolindrom(s.substring(1, s.length() - 1));
+        }
+
+        return false;
     }
 }
