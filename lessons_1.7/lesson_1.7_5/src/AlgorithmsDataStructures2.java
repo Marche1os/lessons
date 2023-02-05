24853653dbbbd9b36cf3fa40b35d177f3d38f2b8
marchelos <markkolp10@gmail.com>
2023-02-05 11:21:25 +0300

    ASD. Lesson 1_7.5. Generation BBST Array. Rewrite implementation

--- lessons_1.7/lesson_1.7_5/src/AlgorithmsDataStructures2.java
+++ lessons_1.7/lesson_1.7_5/src/AlgorithmsDataStructures2.java
@@ -1,27 +1,83 @@
 import java.util.*;
+import java.util.function.ToIntFunction;
 
 public class AlgorithmsDataStructures2 {
-    private static int[] result;
-    private static int count = 0;
 
     public static int[] GenerateBBSTArray(int[] a) {
-        Arrays.sort(a);
-        result = new int[a.length];
-        count = 0;
-        generate(a, 0, a.length - 1);
+        if (a.length == 0)
+            return new int[0];
 
-        return result;
+        Arrays.sort(a);
+
+        ArrayList<Integer> tree = generate(new ArrayList<>(), a, 0, a.length - 1);
+
+        return generateBst(tree);
     }
 
-    private static void generate(int[] array, int start, int end) {
+    private static ArrayList<Integer> generate(
+            final ArrayList<Integer> tree,
+            final int[] sourceArray,
+            int start,
+            int end
+    ) {
         if (start > end)
-            return;
+            return null;
         int middle = (start + end) / 2;
-        int root = array[middle];
+        int root = sourceArray[middle];
 
-        result[count++] = root;
-        generate(array, start, middle - 1);
-        generate(array, middle + 1, end);
+        tree.add(root);
+        generate(tree, sourceArray, start, middle - 1);
+        generate(tree, sourceArray, middle + 1, end);
 
+        return tree;
+    }
+
+    private static int[] generateBst(
+            final ArrayList<Integer> sourceTree
+    ) {
+        Integer[] arrayToComplete = new Integer[sourceTree.size()];
+        arrayToComplete[0] = sourceTree.get(0);
+        for (int i = 1; i < sourceTree.size(); i++) {
+            int index = 0;
+            int node = sourceTree.get(i);
+
+            while (true) {
+                int leftParentIndex = 2 * index + 1;
+                int rightParentIndex = 2 * index + 2;
+                int current = arrayToComplete[index];
+
+                if (leftParentIndex >= arrayToComplete.length || rightParentIndex >= arrayToComplete.length)
+                    return convertPrimitiveToObjectInt(arrayToComplete);
+
+                if (arrayToComplete[leftParentIndex] != null && current > node)
+                    index = leftParentIndex;
+
+                if (arrayToComplete[leftParentIndex] == null && current > node) {
+                    index = leftParentIndex;
+                    arrayToComplete[index] = node;
+                    break;
+                }
+
+                if (arrayToComplete[rightParentIndex] != null && current < node)
+                    index = rightParentIndex;
+
+                if (arrayToComplete[rightParentIndex] == null && current < node) {
+                    index = rightParentIndex;
+                    arrayToComplete[index] = node;
+                    break;
+                }
+            }
+        }
+
+        return convertPrimitiveToObjectInt(arrayToComplete);
+    }
+
+    private static int[] convertPrimitiveToObjectInt(Integer[] array) {
+        int[] result = new int[array.length];
+
+        for (int i = 0; i < array.length; i++)
+            result[i] = array[i];
+
+        return result;
     }
 }
