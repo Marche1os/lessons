2e8f3b19b97ba203aa726313e3624973e1de6998
marche1os <kolpmark@yandex.ru>
2023-06-03 14:26:25 +0300

    исправил логическую ошибку в функции

--- fsharp/strings/task.fs
+++ fsharp/strings/task.fs
@@ -16,5 +16,5 @@ let rec isIthChar = function
 let rec occFromIth = function
     | (s, -1, c) -> 0
     | (s, n, c) when n >= String.length(s) -> 0
-    | (s, n, c) when s.[n] = c -> 1 + occFromIth(s, n - 1, c)
-    | (s, n, c) -> occFromIth(s, n-1, c)
+    | (s, n, c) when s.[n] = c -> 1 + occFromIth(s, n + 1, c)
+    | (s, n, c) -> occFromIth(s, n+1, c)
