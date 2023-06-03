138b461185ed775356162a0331ca2b4bcf2b4487
marche1os <kolpmark@yandex.ru>
2023-06-03 14:12:36 +0300

    fix exception in function isIthChar

--- fsharp/strings/task.fs
+++ fsharp/strings/task.fs
@@ -8,11 +8,13 @@ let rec pow = function
 
 // 17.2
 let rec isIthChar = function
-    | (s: string, n, c:char) when s.[n] = c -> true
+    | (s, n, c) when n < 0 || n >= String.length(s) -> false
+    | (s, n, c) when s.[n] = c -> true
     | _ -> false
 
 // 17.3
 let rec occFromIth = function
     | (s, -1, c) -> 0
-    | (s: string, n, c) when s.[n] = c -> 1 + occFromIth(s, n - 1, c)
+    | (s, n, c) when n >= String.length(s) -> 0
+    | (s, n, c) when s.[n] = c -> 1 + occFromIth(s, n - 1, c)
     | (s, n, c) -> occFromIth(s, n-1, c)
